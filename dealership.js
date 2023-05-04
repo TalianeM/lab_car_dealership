584ceacb67f5a633c4e6f530ea5989136956cf09
Taliane Moussassa <talianegloire392@gmail.com>
2023-05-04 16:34:39 +0100

    adds addCarToStock method in dealership class

--- dealership.js
+++ dealership.js
@@ -1,6 +1,8 @@
 
 // Function
 
+const Car = require("./car");
+
 const Dealership = function(name, maxNoOfCars, stock){
     this.name = name;
     this.maxNoOfCars = maxNoOfCars;
@@ -12,6 +14,45 @@ const Dealership = function(name, maxNoOfCars, stock){
 Dealership.prototype.countNoOfCars = function(){
     return this.maxNoOfCars.length;
 };
+
+// add a car to stock
+
+Dealership.prototype.addCarToStock = function(){
+    return this.addCartoStock.push(Car);
+};
+
+// Return an array containing each car's manufacturer
+
+    // findAllManufacturers
+    // get car object which contains manufacturer property
+    // use .map to source manufacturer property
+    // return this.car.map 
+
+const findAllManufacturers = stock.map((manufacturer) => {
+    return this.manufacturer;
+})
+    
+// Find all the cars from a given manufacturer
+
+    // findCarManufacturer 
+    // get car object which contains manufacturer property
+    // use .filter to source manufacturer property
+
+const findCarManufacturer = stock.filter((manufacturer)=> {
+    return this.manufacturer;
+})
+
+// Find total value of all cars in stock
+
+
+
+// Total car value 
+
+    // get car object which contains price property
+    // reduce to grab sum of price value 
+    
+
+
 
 
 // exports Dealership module
