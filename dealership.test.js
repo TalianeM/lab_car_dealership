4c89ddaf635801eab092131995af94637965f1ac
Taliane Moussassa <talianegloire392@gmail.com>
2023-05-05 09:11:58 +0100

    adds count number of cars test

--- dealership.test.js
+++ dealership.test.js
@@ -1,5 +1,46 @@
 
 // imports dealership class 
 
+const { beforeEach } = require("node:test");
 const Dealership = require("./dealership");
 
+beforeEach
+
+// Test 1
+
+test('can count number of cars ',() => {
+const expected = 3; 
+const actual = dealership.countNoOfCars
+expected(actual).toBe(expected);
+
+});
+
+// // Test 2
+
+// test('can add a car to stock ',() => {
+
+// });
+
+// // Test 3
+
+// test('can find all the cars manufacturer ',() => {
+
+// });
+
+// // Test 4
+
+// test('can find cars from given manufacturer ',() => {
+
+// });
+
+// // Test 5
+
+// test('can find price of cars ',() => {
+
+// });
+
+// // Test 6
+
+// test('can find total value of cars in stock ',() => {
+
+// });
