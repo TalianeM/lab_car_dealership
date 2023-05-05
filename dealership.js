979c7fa817ed49c28a2ec901c0aaedd9c27dabc5
Taliane Moussassa <talianegloire392@gmail.com>
2023-05-05 07:42:04 +0100

    stock property redefined to carStock

--- dealership.js
+++ dealership.js
@@ -6,7 +6,7 @@ const Car = require("./car");
 const Dealership = function(name, maxNoOfCars){
     this.name = name;
     this.maxNoOfCars = maxNoOfCars;
-    this.stock = [];
+    this.carStock = [];
 };
 
 // Count number of cars
@@ -15,41 +15,41 @@ Dealership.prototype.countNoOfCars = function(){
     return this.maxNoOfCars.length;
 };
 
-// add a car to stock
+// // add a car to stock
 
-Dealership.prototype.addCarToStock = function(car){
-    return this.stock.push(car);
-};
+// Dealership.prototype.addCarToStock = function(car){
+//     return this.stock.push(car);
+// };
 
-// Return an array containing each car's manufacturer
+// // Return an array containing each car's manufacturer
 
-    // findAllManufacturers
-    // get car object which contains manufacturer property
-    // use .map to source manufacturer property
-    // return this.car.map 
+//     // findAllManufacturers
+//     // get car object which contains manufacturer property
+//     // use .map to source manufacturer property
+//     // return this.car.map 
 
-Dealership.prototype.findAllManufacturers = function(){ 
-    return this.stock.map((car) => {
-        return car.manufacturer;
+// Dealership.prototype.findAllManufacturers = function(){ 
+//     return this.stock.map((car) => {
+//         return car.manufacturer;
     
-})
-}  
-// Find all the cars from a given manufacturer
+// })
+// }  
+// // Find all the cars from a given manufacturer
 
-    // findCarManufacturer 
-    // get car object which contains manufacturer property
-    // use .filter to source manufacturer property
+//     // findCarManufacturer 
+//     // get car object which contains manufacturer property
+//     // use .filter to source manufacturer property
 
-const findCarManufacturer = stock.filter((manufacturer)=> {
-    return this.manufacturer;
-})
+// const findCarManufacturer = stock.filter((manufacturer)=> {
+//     return this.manufacturer;
+// })
 
-// Find total value of all cars in stock
+// // Find total value of all cars in stock
 
-const totalStockValue = stock.reduce((accumulator, price) => {
-        return accumulator + Car.price;  // why capital C?
-}, 0)
+// const totalStockValue = stock.reduce((accumulator, price) => {
+//         return accumulator + Car.price;  // why capital C?
+// }, 0)
 
-// exports Dealership module
+// // exports Dealership module
 
-module.exports = Dealership;
+// module.exports = Dealership;
