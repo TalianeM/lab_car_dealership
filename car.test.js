350c102c538f3e37cce5ed9d73aa1ac4843874b7
Taliane Moussassa <talianegloire392@gmail.com>
2023-05-05 08:53:37 +0100

    updates car test

--- car.test.js
+++ car.test.js
@@ -2,3 +2,23 @@
 // imports car class
 
 const Car = require("./car");
+
+ // Test 1 
+
+ 
+test(' can get car manufacturer ',() => { 
+
+}); 
+
+// Test 1 
+
+ 
+test('can get car price ',() => { 
+
+}); 
+
+// Test 1 
+
+test('can get car manufacturer ',() => { 
+
+}); 
